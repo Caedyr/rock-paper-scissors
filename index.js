54e19b206a0dcbfbdbee6788db304d32997c614c
Caedyr <130426718+Caedyr@users.noreply.github.com>
2023-05-05 15:44:45 -0400

    Set up the playRound function

--- index.js
+++ index.js
@@ -9,12 +9,27 @@ function getComputerChoice () {
 }
 // Request a user selection
 // Send the user an input request for Rock, Paper or Scissors.
-
+    const input = prompt("Choose Rock, Paper or Scissors!");
+    const playerChoice = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+    console.log(input);
+    console.log(playerChoice);
 // Compare the two selections
 // Take the ComputerChoice and PlayerSelection. Compare the two - If they are ===, its a tie.
 // Rock beats scissors, scissors beats paper, paper beats rock.
 function playRound () {
+    let computerChoice = getComputerChoice();
+    if (computerChoice === playerChoice) {
+        return "It's a tie!";
+    }
+    else if (playerChoice !== "Rock" && playerChoice !== "Paper" && playerChoice !== "Scissors") {
+        return "That's not a valid option, silly!";
+    }
 
+    else {
+        switch (computerChoice, playerChoice) {
+            
+        }
+    }
 }
 
-console.log(getComputerChoice());
+console.log(playRound());
