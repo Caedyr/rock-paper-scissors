f1f4dd2bb87257ff05f71eaa9dd308f3da19104a
Caedyr <130426718+Caedyr@users.noreply.github.com>
2023-05-05 15:59:12 -0400

    Finish logic for a single round

--- index.js
+++ index.js
@@ -11,13 +11,16 @@ function getComputerChoice () {
 // Send the user an input request for Rock, Paper or Scissors.
     const input = prompt("Choose Rock, Paper or Scissors!");
     const playerChoice = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
-    console.log(input);
-    console.log(playerChoice);
 // Compare the two selections
 // Take the ComputerChoice and PlayerSelection. Compare the two - If they are ===, its a tie.
 // Rock beats scissors, scissors beats paper, paper beats rock.
 function playRound () {
     let computerChoice = getComputerChoice();
+    console.log(playerChoice);
+    console.log(computerChoice);
+    console.log(typeof(playerChoice));
+    console.log(typeof(computerChoice));
+
     if (computerChoice === playerChoice) {
         return "It's a tie!";
     }
@@ -27,7 +30,27 @@ function playRound () {
 
     else {
         switch (computerChoice, playerChoice) {
-            
+            case "Rock", "Paper":
+                return "You win!";
+            break;
+            case "Rock", "Scissors":
+                return "You lose!";
+            break;
+            case "Paper", "Rock":
+                return "You lose!";
+            break;
+            case "Paper", "Scissors":
+                return "You win!";
+            break;
+            case "Scissors", "Rock":
+                return "You win!";
+            break;
+            case "Scissors", "Paper":
+                return "You lose!";
+            break;
+            default:
+                return "oops"
+            break;
         }
     }
 }
